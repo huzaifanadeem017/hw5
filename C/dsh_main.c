f6ca318c11ba358e5e601e1eda86b04b21fd2a0e
Huzaifa Nadeem <hnadee2@uic.edu>
2022-03-03 11:18:50 -0600

    pushing

--- C/dsh_main.c
+++ C/dsh_main.c
@@ -5,19 +5,64 @@
 #include<fcntl.h>
 #include<sys/stat.h>
 #include<sys/types.h>
-
+#include<ncurses.h>
 #include"dsh.h"
 int main(int argc, char** argv) {
     char *line=0;
     size_t size=0;
+    int height,width,startY,startX;
 
-    dsh_init();
+    height=30;
+    width=50;
+    startY=5;
+    startX=5;
+
+    
+
+
+   dsh_init(); 
+
     printf("dsh> ");
-
+	
+    
     while(getline(&line,&size,stdin) > 0) {
 
-        dsh_run(line);
+       // dsh_run(line);
+	printf("dsh>c");
 
-        printf("dsh> ");
+
+
+	initscr();
+	noecho();
+	cbreak();
+
+	WINDOW *inpWindow=newwin(height,width,startY,startX);
+	box(inpWindow,0,0);
+	mvwprintw(inpWindow,1,1,"Enter Input");
+	dsh_run(line);
+	mvwprintw(inpWindow,3,3,line);
+
+	refresh();
+	wrefresh(inpWindow);
+	int inp=wgetch(inpWindow);
+
+
+	initscr();	
+	WINDOW *myWindow=newwin(height,width,startY,startX);
+	refresh();
+	box(myWindow,'*','*');
+	mvwprintw(myWindow,1,1,"History");
+	mvwprintw(myWindow,3,3,line);
+	wrefresh(myWindow);
+	
+        int c=getch();
+
+        
+  
+   
+        endwin();	   
    }
+	
+   
+    return 0;
 }
